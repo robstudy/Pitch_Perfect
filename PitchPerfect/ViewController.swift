c59e90114ddce04a467eac47da88734f514c723c
Robert Garza <robstudent1988@gmail.com>
2017-12-10 21:20:45 -0800

    Created IBAction's for recordAudio and stopRecordingAudio, currently only changes the text of the recordingLabel

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(_ sender: AnyObject) {
+        recordingLabel.text = "Recording in Progress"
+    }
 
+    @IBAction func stopRecordingAudio(_ sender: Any) {
+        recordingLabel.text = "Tap to Record"
+    }
+    
 }
 
